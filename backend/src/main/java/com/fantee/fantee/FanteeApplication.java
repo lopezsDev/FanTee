e437ac6c40ea37d32065caaf6d5e509dafc136c7
User <lopezs.dev@gmail.com>
2025-07-26 19:07:06 -0600

    chore(backend): initial backend setup with Docker, SQL Server and basic project structure

---
+++ backend/src/main/java/com/fantee/fantee/FanteeApplication.java
@@ -0,0 +1,13 @@
+package com.fantee.fantee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanteeApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/fantee/fantee/FanteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantee.fantee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
